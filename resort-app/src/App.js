da49124e947c291483c92bc9780c2eac6472d843
Martuza <martuza.shimul@gmail.com>
2020-01-26 07:26:56 +0600

    react-router-dom configured

--- resort-app/src/App.js
+++ resort-app/src/App.js
@@ -6,15 +6,17 @@ import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
 
+import { Route, Switch } from "react-router-dom";
+
 function App() {
 	return (
 		<>
-			<h1>
-				<Home></Home>
-				<Rooms></Rooms>
-				<SingleRoom></SingleRoom>
-				<Error></Error>
-			</h1>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/rooms" component={Rooms} />
+				<Route exact path="/rooms/:slug" component={SingleRoom} />
+				<Route component={Error} />
+			</Switch>
 		</>
 	);
 }
